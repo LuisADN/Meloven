050216949968a9f8c0ff907dca8e1b045e817d79
Luis Ramos <luiscarlosadn@gmail.com>
2018-06-07 00:42:57 +0200

    Added Note change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+/* ********************** FORM ACTION CHANGE ********************** */
+/* **************************************************************** */
 function RadioStateChange(x) {
-    let form = document.getElementById('mc-embedded-subscribe-form');
+    var form = document.getElementById('mc-embedded-subscribe-form');
     if ( x == 0) {
         form.action = 'https://meloven.us18.list-manage.com/subscribe/post?u=4d2f69fba82cf20af4f478eb7&amp;id=76c0a7833d';
     }
@@ -7,7 +9,8 @@ function RadioStateChange(x) {
         form.action = 'https://meloven.us18.list-manage.com/subscribe/post?u=4d2f69fba82cf20af4f478eb7&amp;id=34470ec719';
     }
 }
-
+/* ********************** MUSIC PLAYER ********************** */
+/* ********************************************************** */
 document.getElementById('lecteur').addEventListener('click', function (ev) {
     document.getElementById('video').play();
     document.getElementById('lecteur').style.display = "none";
@@ -32,3 +35,33 @@ videoPlayer.addEventListener('click', function () {
         document.getElementById('lecteur').style.display = "none";
     }
 });
+/* ********************** RADIO BUTTON NOTE CHANGE ********************** */
+/* ********************************************************************** */
+document.getElementById('contactChoice2').addEventListener('click', function (ev) {
+    var nullOrNot = document.querySelector('#liChoice2 .check img');
+    if( nullOrNot == null){
+        var parent = document.querySelector('#liChoice2 .check');
+        var sourceTwo = document.createElement('img');
+        sourceTwo.setAttribute('src', 'img/note.svg');
+        sourceTwo.setAttribute('alt', 'Note');
+        parent.append(sourceTwo);
+
+        var removeChoiseOne = document.querySelector('#liChoice1 .check');
+        var sourceOne = document.querySelector('#liChoice1 .check img');
+        removeChoiseOne.removeChild(sourceOne);
+    }
+})
+document.getElementById('contactChoice1').addEventListener('click', function (ev) {
+    var nullOrNot = document.querySelector('#liChoice1 .check img');
+    if( nullOrNot == null){
+        var parent = document.querySelector('#liChoice1 .check');
+        var sourceOne = document.createElement('img');
+        sourceOne.setAttribute('src', 'img/note.svg');
+        sourceOne.setAttribute('alt', 'Note');
+        parent.append(sourceOne);
+
+        var removeChoiseTwo = document.querySelector('#liChoice2 .check');
+        var sourceTwo = document.querySelector('#liChoice2 .check img');
+        removeChoiseTwo.removeChild(sourceTwo);
+    }
+})
